d346480eeb713f640f1d73593a791d5257428a02
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-06-25 17:25:21 +0800

    Add tests

---
+++ index.test.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+const app = require("./index");
+
+describe("My awesome service", () => {
+  it("should get a greeting", () => {
+    return request(app)
+      .get("/")
+      .expect("Hello World! This app is live!");
+  });
+});
